bc1e329e2844ff34932a5fa3351b3d8f7adfc43c
Keshav Mathur <keshav37a@gmail.com>
2020-05-02 11:11:24 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const port = 8000;
+
+const app = new express();
+app.listen(port, function(err){
+    if(err) { console.log(`${err}`); }
+
+    console.log(`app up and running on port ${port}`);
+})
