a473d7c775f0d71638c7ffbfaed8db3709c12e37
Keshav Mathur <keshav37a@gmail.com>
2020-05-02 13:18:05 +0530

    add layout for sign-in and sign-up

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -2,3 +2,13 @@ module.exports.user = function(req, res){
     console.log('user in user_controller called');
     return res.send('<h1>User</h1>');
 }
+
+module.exports.signInRender = function(req, res){
+    let title = "sign-in";
+    return res.render('sign-in', {title:title});
+}
+
+module.exports.signUpRender = function(req, res){
+    let title = "sign-up";
+    return res.render('sign-up', {title:title});
+}
--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const usersController = require('../controllers/user_controller');
 
 router.get('/', usersController.user);
+router.get('/sign-in', usersController.signInRender);
+router.get('/sign-up', usersController.signUpRender);
 
 
 module.exports = router;
