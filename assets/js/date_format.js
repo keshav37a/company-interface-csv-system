d4f42eaa7eedff1dd3c2470501b1e92526666843
Keshav Mathur <keshav37a@gmail.com>
2020-05-05 23:41:09 +0530

    add color coded results in students page

--- assets/js/date_format.js
+++ assets/js/date_format.js
@@ -1,16 +1,16 @@
 let dateFormatFn = function(dateString){
-    console.log('DateFormat fn called');
-    console.log(dateString);
+    // console.log('DateFormat fn called');
+    // console.log(dateString);
     let formattedDate = moment(dateString).format('MMMM DD YYYY');
     return formattedDate;
 }
 
 let dates = $(".date");
 for(let i=0; i<dates.length; i++){
-    console.log(dates[i].innerHTML);
+    // console.log(dates[i].innerHTML);
     let unformattedDate = dates[i].innerHTML;
-    console.log(unformattedDate);
-    console.log(1);
+    // console.log(unformattedDate);
+    // console.log(1);
     let formattedDate = dateFormatFn(unformattedDate);
     dates[i].innerHTML = formattedDate;
     // console.log(formattedDate);
--- assets/js/list_students.js
+++ assets/js/list_students.js
@@ -1,2 +1,25 @@
 console.log('students_list script called');
 
+let statuses = $('.status');
+
+statuses.each(function(){
+    let el = $(this);
+    if(el.text().toLowerCase()=='pass'){
+        el.css('color', 'green');
+    }
+    else if(el.text().toLowerCase()=='fail' || el.text().toLowerCase()=="didn't attempt"){
+        el.css('color', 'red');
+    }
+    else{
+        el.css('color', 'blue');
+    }
+})
+
+// for(let i=0; i<statuses.length; i++){
+//     let statusObj = statusObj[i];
+//     let statusObjString = statusObj.innerHTML;
+//     if(statusObjString.toLowerCase()=='pass'){
+//         $(statusObj).css("color", "green");
+//     }
+// }
+// console.log(statuses);
